8e98afed4279c691222f9c8eb928f3a5854340b5
Chanfi Mari Yram <152385957+JusDeFruits1@users.noreply.github.com>
2025-04-09 14:17:09 +0200

    Initiation à Twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,23 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function HomePage(): Response
     {
-        return new Response('<strong>Hello World ! </strong>');
+        $starshipCount = 457;
+        $myShip = [
+            'name' => 'X-Wing',
+            'class' => 'Starfighter',
+            'captain' => 'Jean Luc Pickles',
+            'status' => 'active',
+        ];
+
+        return $this->render('main/homepage.html.twig', ['numberOfStarships' => $starshipCount, 'myShip' => $myShip]);
     }
 }
