9e7ec1bf681afd500d2909e0e600a3b57fa715cd
Chanfi Mari Yram <152385957+JusDeFruits1@users.noreply.github.com>
2025-04-16 14:09:30 +0200

    Générer des URL

---
+++ src/Controller/StarshipController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StarshipRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StarshipController extends AbstractController
+{
+    #[Route('/starships/{id<\d+>}', name: 'app_starship_show')]
+    public function show(int $id, StarshipRepository $repository): Response
+    {
+        $starship = $repository->find($id);
+
+        return $this->render('starship/show.html.twig', ['ship' => $starship]);
+    }
+}
